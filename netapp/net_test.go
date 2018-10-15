501430620a5c6cbbe9fc43e8166d4e93ec3fe5f6
Stepan Mazurov <stepan.mazurov@factioninc.com>
2018-10-15 17:01:57 -0400

    fix network get iter to not pass empty interface name w/test

--- netapp/net_test.go
+++ netapp/net_test.go
@@ -28,6 +28,54 @@ func TestNet_NetInterfaceCreateSuccess(t *testing.T) {
 	checkResponseSuccess(&call.Results.SingleResultBase, err, t)
 }
 
+func TestNet_NetInterfaceGetIterSuccess(t *testing.T) {
+	c, teardown := createTestClientWithFixtures(t)
+	defer teardown()
+
+	o := &netapp.NetInterfaceInfo{
+		Vserver: "T100",
+	}
+	opts := &netapp.NetInterfaceOptions{
+		Query: &netapp.NetInterfaceQuery{
+			NetInterfaceInfo: o,
+		},
+		MaxRecords: 20,
+	}
+	result, _, err := c.Net.NetInterfaceGetIter(opts)
+
+	checkResponseSuccess(&result.Results.ResultBase, err, t)
+
+	info := result.Results.AttributesList.NetInterfaceAttributes[0]
+
+	tests := []struct {
+		name string
+		got  interface{}
+		want interface{}
+	}{
+		{"Number of results", result.Results.NumRecords, 3},
+		{"Address", info.Address, "172.30.1.1"},
+		{"Admin Status", info.AdministrativeStatus, "up"},
+		{"Comment", info.Comment, "-"},
+		{"Current Node", info.CurrentNode, "test-cluster01-01"},
+		{"Current Port", info.CurrentPort, "a0a-4000"},
+		{"Data Protocols", info.DataProtocols, &[]string{"nfs"}},
+		{"DNS Domain Name", info.DnsDomainName, "none"},
+		{"Home Node", info.HomeNode, "test-cluster01-01"},
+		{"Home Port", info.HomePort, "a0a-4000"},
+		{"Netmask", info.Netmask, "255.255.255.0"},
+		{"VServer", info.Vserver, "T100"},
+		{"Role", info.Role, "data"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if !reflect.DeepEqual(tt.got, tt.want) {
+				t.Errorf("NetInterface.GetIter() got = %+v, want %+v", tt.got, tt.want)
+			}
+		})
+	}
+}
+
 func TestNet_NetInterfaceDeleteSuccess(t *testing.T) {
 	c, teardown := createTestClientWithFixtures(t)
 	defer teardown()
