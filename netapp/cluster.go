8f1fffaab6cb4a20a28309778f04dc24fc24ab2d
Jennings Liu <jennings.liu@sap.com>
2019-07-11 20:05:11 +0800

    remove identity contact

--- netapp/cluster.go
+++ netapp/cluster.go
@@ -16,7 +16,7 @@ type Cluster struct {
 
 
 type ClusterInfo struct {
-	ClusterContact          *ClusterContactInfo     `xml:"cluster-contact,omitempty"`
+	ClusterContact          string `xml:"cluster-contact,omitempty"`
 	ClusterLocation         string `xml:"cluster-location"`
 	ClusterName  					  int    `xml:"cluster-name"`
 	ClusterSerialNumber 	  string `xml:"cluster-serial-number"`
@@ -38,54 +38,8 @@ type  ClusterResponse struct {
 
 func (c *Cluster) List(options *ClusterOptions) (*ClusterResponse, *http.Response, error) {
 	c.Params.XMLName = xml.Name{Local: "cluster-identity-get"}
-	c.Params.NodeDetailOptions = options
+	c.Params.ClusterOptions = options
 	r := ClusterResponse{}
-	res, err := s.get(s, &r)
-	return &r, res, err
-}
-
-
-type ClusterClusterContact struct {
-	Base
-	Params struct {
-		XMLName xml.Name
-		*ClusterContactOptions
-	}
-}
-
-
-type ClusterContactInfo struct {
-	address 				  string 
-	business-name	 		string 
-	city 						  string 
-	country						string 
-	primary-alt-phone	string 
-	primary-email			string
-	primary-name			string 
-	primary-phone			string 
-	secondary-alt-phone		string 
-	secondary-email				string 
-	secondary-name				string 	
-	secondary-phone				string 	
-	state									string 
-	zip-code							string 
-}
-
-
-
-type  ClusterContactOptions struct {
-	DesiredAttributes *ClusterContactInfo `xml:"desired-attributes,omitempty"`
-}
-
-type   ClusterContactResponse struct {
-	DesiredAttributes *ClusterContactInfo `xml:"desired-attributes,omitempty"`
-}
-
-
-func (c *ClusterClusterContact) List(options *ClusterContactOptions) (*ClusterContactResponse, *http.Response, error) {
-	c.Params.XMLName = xml.Name{Local: "cluster-contact-get"}
-	c.Params.ClusterContactOptions = options
-	r := ClusterResponse{}
-	res, err := s.get(s, &r)
+	res, err := c.get(c, &r)
 	return &r, res, err
 }
