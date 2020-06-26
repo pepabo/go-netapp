4c6b431d369de47f56a0c264eac510659d64ad35
Roger Zimmermann <roger.zimmermann@inventx.ch>
2020-06-26 14:13:24 +0200

    fixed remove

--- netapp/vserver-exports.go
+++ netapp/vserver-exports.go
@@ -37,6 +37,29 @@ type VServerExportsResponse struct {
 	} `xml:"results"`
 }
 
+type VServerExportRuleListResponse struct {
+	XMLName xml.Name `xml:"netapp"`
+	Results struct {
+		ResultBase
+		AttributesList struct {
+			VserverExportRuleInfo []VServerExportRuleInfo `xml:"export-rule-info"`
+		} `xml:"attributes-list"`
+	} `xml:"results"`
+}
+
+// ListExportRules list the rules of an export policy
+func (v Vserver) ListExportRules(vServerName string, exportPolicy string, options *VServerExportRuleInfo) (*VServerExportRuleListResponse, *http.Response, error) {
+	v.Name = vServerName
+	v.Params.XMLName = xml.Name{Local: "export-rule-get-iter"}
+	v.Params.VServerExportRuleInfo = VServerExportRuleInfo{
+		PolicyName: exportPolicy,
+	}
+
+	r := VServerExportRuleListResponse{}
+	res, err := v.get(v, &r)
+	return &r, res, err
+}
+
 // CreateExportRule creates a new export rule for a given vserver
 func (v VServer) CreateExportRule(vServerName string, options *VServerExportRuleInfo) (*VServerExportsResponse, *http.Response, error) {
 	req := v.newVServerExportsRequest()
@@ -57,21 +80,6 @@ func (v VServer) DeleteExportRule(vServerName string, policyName string, ruleInd
 	req.Params.VServerExportRuleInfo = VServerExportRuleInfo{
 		PolicyName: policyName,
 		RuleIndex:  ruleIndex,
-	}
-
-	r := &VServerExportsResponse{}
-	res, err := v.get(req, r)
-	return r, res, err
-}
-
-// DeleteExportRule removes an export rule for a given vserver, policy and rule index
-func (v VServer) DeleteExportRuleByClientMatch(vServerName string, policyName string, clientMatch string) (*VServerExportsResponse, *http.Response, error) {
-	req := v.newVServerExportsRequest()
-	req.Base.Name = vServerName
-	req.Params.XMLName = xml.Name{Local: "export-rule-destroy"}
-	req.Params.VServerExportRuleInfo = VServerExportRuleInfo{
-		PolicyName:  policyName,
-		ClientMatch: clientMatch,
 	}
 
 	r := &VServerExportsResponse{}
