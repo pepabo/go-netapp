a320a9f21963ae43703e2e9ef408dff4998f2c97
Roger Zimmermann <roger.zimmermann@inventx.ch>
2020-07-06 17:05:49 +0200

    initial ci

---
+++ netapp/lun_operations.go
@@ -0,0 +1,39 @@
+package netapp
+
+import (
+	"encoding/xml"
+	"net/http"
+)
+
+// LUN Operations
+
+const (
+	LUNOfflineOperation = "lun-offline"
+	LUNDestroyOperation = "lun-destroy"
+)
+
+type LunOperation struct {
+	Base
+	Params struct {
+		XMLName xml.Name
+		LunPath *lunPath
+	}
+}
+
+type lunPath struct {
+	XMLName xml.Name
+	Path    string `xml:",innerxml"`
+}
+
+func (l LunOperation) Operation(vserverName string, lunPathName string, operation string) (*SingleResultResponse, *http.Response, error) {
+	l.Params.XMLName = xml.Name{Local: operation}
+	l.Name = vserverName
+	elementName := "name"
+	l.Params.LunPath = &lunPath{
+		XMLName: xml.Name{Local: elementName},
+		Path:    lunPathName,
+	}
+	r := SingleResultResponse{}
+	res, err := l.get(l, &r)
+	return &r, res, err
+}
