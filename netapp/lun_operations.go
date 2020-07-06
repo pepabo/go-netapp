40ebfb3d0cc38e01da21ef6575e358208f1430fe
Roger Zimmermann <roger.zimmermann@inventx.ch>
2020-07-06 23:21:27 +0200

    added Map

--- netapp/lun_operations.go
+++ netapp/lun_operations.go
@@ -17,8 +17,9 @@ const (
 type LunOperation struct {
 	Base
 	Params struct {
-		XMLName xml.Name
-		LunPath *lunPath
+		XMLName           xml.Name
+		LunPath           *lunPath
+		LunInitiatorGroup *lunInitiatorGroup
 		LunCreateOptions
 	}
 }
@@ -28,10 +29,16 @@ type lunPath struct {
 	Path    string `xml:",innerxml"`
 }
 
+type lunInitiatorGroup struct {
+	XMLName        xml.Name
+	InitiatorGroup string `xml:",innerxml"`
+}
+
 type LunCreateOptions struct {
 	Class                   string `xml:"class,omitempty"`
 	Comment                 string `xml:"comment,omitempty"`
 	ForeignDisk             string `xml:"foreign-disk,omitempty"`
+	InitiatorGroup          string `xml:"initiator-group,omitempty"`
 	OsType                  string `xml:"ostype,omitempty"`
 	Path                    string `xml:"path,omitempty"`
 	PrefixSize              string `xml:"prefix-size,omitempty"`
@@ -50,6 +57,20 @@ func (l LunOperation) Create(vserverName string, options *LunCreateOptions) (*Si
 	r := SingleResultResponse{}
 	res, err := l.get(l, &r)
 	return &r, res, err
+}
+
+func (l LunOperation) Map(vserverName string, lunPathName string, initiatorGroup string) (*SingleResultResponse, *http.Response, error) {
+	l.Params.XMLName = xml.Name{Local: LunMapOperation}
+	l.Name = vserverName
+	l.Params.LunInitiatorGroup = &lunInitiatorGroup{
+		XMLName: xml.Name{Local: "initiator-group"},
+		Path:    initiatorGroup,
+	}
+	l.Params.LunPath = &lunPath{
+		XMLName: xml.Name{Local: "path"},
+		Path:    lunPathName,
+	}
+	l.Params.LunPath = lunPath
 }
 
 func (l LunOperation) Operation(vserverName string, lunPathName string, operation string) (*SingleResultResponse, *http.Response, error) {
